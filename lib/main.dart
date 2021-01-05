8ab4bd7342a9b27291f30fd66150631fccc59d10
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2021-01-05 12:20:25 +0530

    get user info with token

--- lib/main.dart
+++ lib/main.dart
@@ -4,73 +4,100 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<void> getAuthorization() async {
-  String url = "https://auth.riotgames.com/api/v1/authorization";
-  final http.Response response = await http.post(
-    url,
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      "client_id":"play-valorant-web-prod",
-      "nonce":"1",
-      "redirect_uri": "https://beta.playvalorant.com/opt_in",
-      "response_type": "token id_token",
-      "scope": "account openid"
-      },
-    ),
-  );
-  print(response.body);
-}
+import 'package:dio/dio.dart';
 
-Future<void> authenticate(String username, String password) async {
-  String url = "https://auth.riotgames.com/api/v1/authorization";
-  final http.Response response = await http.put(
-    url,
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
+class ApiProvider {
+  Dio _dio;
+  String aToken = '';
+
+  final BaseOptions options = new BaseOptions(
+    baseUrl: 'https://auth.riotgames.com/api/v1/authorization',
+    connectTimeout: 15000,
+    receiveTimeout: 13000,
+  );
+  static final ApiProvider _instance = ApiProvider._internal();
+
+  factory ApiProvider() => _instance;
+
+  ApiProvider._internal() {
+    _dio = Dio(options);
+    _dio.interceptors
+        .add(InterceptorsWrapper(onRequest: (Options options) async {
+      // to prevent other request enter this interceptor.
+      _dio.interceptors.requestLock.lock();
+      // We use a new Dio(to avoid dead lock) instance to request token.
+      //Set the cookie to headers
+      options.headers["cookie"] = aToken;
+
+      _dio.interceptors.requestLock.unlock();
+      return options; //continue
+    }));
+  }
+
+  Future<void> authenticate(String username, String password) async {
+    final request = {
       "type": "auth",
       "username": username,
       "password": password
-      },
-    ),
-  );
-  print(response.body);
-}
+    };
+    final response = await _dio.put('/', data: json.encode(request));
+    print("authenticate res ===" + response.data.toString());
+  }
 
-Future<Album> createAlbum(String title) async {
-  final http.Response response = await http.post(
-    'https://jsonplaceholder.typicode.com/albums',
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'title': title,
-    }),
-  );
+  Future<void> getAuthorization() async {
+    final request = {
+      "client_id": "play-valorant-web-prod",
+      "nonce": "1",
+      "redirect_uri": "https://beta.playvalorant.com/opt_in",
+      "response_type": "token id_token",
+      "scope": "account openid"
+    };
+    final response = await _dio.post('/', data: request);
+    //get cooking from response
+    final cookies = response.headers.map['set-cookie'];
+    if (cookies.isNotEmpty && cookies.length == 2) {
+      final authToken = cookies[1]
+          .split(';')[0]; //it depends on how your server sending cookie
+      //save this authToken in local storage, and pass in further api calls.
+      aToken =
+          authToken; //saving this to global variable to refresh current api calls to add cookie.
+      print("authtoken ===" + authToken);
+    }
 
-  if (response.statusCode == 201) {
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to create album.');
+    print("cookies ===" + cookies.toString());
+    //print(response.headers.toString());
+
+    print(response.data.toString());
   }
 }
 
-class Album {
-  final int id;
-  final String title;
-
-  Album({this.id, this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      id: json['id'],
-      title: json['title'],
-    );
-  }
-}
+const Map<String, String> rank = {
+  "0": "Unrated",
+  "1": "Unknown 1",
+  "2": "Unknown 2",
+  "3": "Iron 1",
+  "4": "Iron 2",
+  "5": "Iron 3",
+  "6": "Bronze 1",
+  "7": "Bronze 2",
+  "8": "Bronze 3",
+  "9": "Silver 1",
+  "10": "Silver 2",
+  "11": "Silver 3",
+  "12": "Gold 1",
+  "13": "Gold 2",
+  "14": "Gold 3",
+  "15": "Platinum 1",
+  "16": "Platinum 2",
+  "17": "Platinum 3",
+  "18": "Diamond 1",
+  "19": "Diamond 2",
+  "20": "Diamond 3",
+  "21": "Immortal 1",
+  "22": "Immortal 2",
+  "23": "Immortal 3",
+  "24": "Radiant"
+};
 
 void main() {
   runApp(MyApp());
@@ -86,8 +113,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final TextEditingController _controller = TextEditingController();
-  Future<Album> _futureAlbum;
+  final TextEditingController _controllerBearer = TextEditingController();
+  final TextEditingController _controllerEntitlement = TextEditingController();
+  String _region = 'na';
+  Future<String> _user;
+  Future<List<dynamic>> _matches;
+
+  Future<String> getUserId() async {
+    final http.Response response = await http.post(
+      'https://auth.riotgames.com/userinfo',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ${_controllerBearer.text}',
+      },
+      body: jsonEncode(<String, String>{}),
+    );
+
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      return jsonDecode(response.body)['sub'];
+    } else {
+      throw Exception('Failed to get user id');
+    }
+  }
+
+  Future<List<dynamic>> getCompiDetails(String user) async {
+    final http.Response response = await http.get(
+      'https://pd.$_region.a.pvp.net/mmr/v1/players/$user/competitiveupdates?startIndex=0&endIndex=20',
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${_controllerBearer.text}',
+        'X-Riot-Entitlements-JWT': '${_controllerEntitlement.text}',
+      },
+    );
+    print(response.statusCode);
+    print(jsonDecode(response.body));
+
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      return jsonDecode(response.body)['Matches'];
+    } else {
+      throw Exception('Failed to get compi details');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -103,32 +169,62 @@ class _MyAppState extends State<MyApp> {
         body: Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.all(8.0),
-          child: (_futureAlbum == null)
+          child: (_user == null)
               ? Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     TextField(
-                      controller: _controller,
-                      decoration: InputDecoration(hintText: 'Enter Title'),
+                      controller: _controllerBearer,
+                      decoration: InputDecoration(
+                          hintText: 'Enter Bearer Authentication Token'),
+                    ),
+                    TextField(
+                      controller: _controllerEntitlement,
+                      decoration:
+                          InputDecoration(hintText: 'Entitlement Token'),
+                    ),
+                    DropdownButton(
+                      isExpanded: true,
+                      value: _region,
+                      items: [
+                        DropdownMenuItem(
+                          child: Text('Asia'),
+                          value: 'ap',
+                        ),
+                        DropdownMenuItem(
+                          child: Text('North America'),
+                          value: 'na',
+                        ),
+                        DropdownMenuItem(
+                          child: Text('Europe'),
+                          value: 'eu',
+                        ),
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _region = value;
+                        });
+                      },
+                      icon: Icon(Icons.map),
                     ),
                     ElevatedButton(
-                      child: Text('Create Data'),
-                      onPressed: () {
+                      child: Text('Check Elo'),
+                      onPressed: () async {
                         setState(() {
+                          _user = getUserId();
                           print("clicked");
-                          getAuthorization();
-                          authenticate("pedroman05","pedro2002");
-                          // _futureAlbum = createAlbum(_controller.text);
                         });
+                        _matches = getCompiDetails(await _user);
+                        setState(() {});
                       },
                     ),
                   ],
                 )
-              : FutureBuilder<Album>(
-                  future: _futureAlbum,
+              : FutureBuilder<String>(
+                  future: _user,
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
-                      return Text(snapshot.data.title);
+                      return Text(snapshot.data);
                     } else if (snapshot.hasError) {
                       return Text("${snapshot.error}");
                     }
