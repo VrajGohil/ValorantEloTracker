7686fd283f82cbe044c2b692a18e04b79a4e78d9
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2021-01-06 12:57:25 +0530

    backend complete

--- lib/main.dart
+++ lib/main.dart
@@ -4,74 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import 'package:dio/dio.dart';
-
-class ApiProvider {
-  Dio _dio;
-  String aToken = '';
-
-  final BaseOptions options = new BaseOptions(
-    baseUrl: 'https://auth.riotgames.com/api/v1/authorization',
-    connectTimeout: 15000,
-    receiveTimeout: 13000,
-  );
-  static final ApiProvider _instance = ApiProvider._internal();
-
-  factory ApiProvider() => _instance;
-
-  ApiProvider._internal() {
-    _dio = Dio(options);
-    _dio.interceptors
-        .add(InterceptorsWrapper(onRequest: (Options options) async {
-      // to prevent other request enter this interceptor.
-      _dio.interceptors.requestLock.lock();
-      // We use a new Dio(to avoid dead lock) instance to request token.
-      //Set the cookie to headers
-      options.headers["cookie"] = aToken;
-
-      _dio.interceptors.requestLock.unlock();
-      return options; //continue
-    }));
-  }
-
-  Future<void> authenticate(String username, String password) async {
-    final request = {
-      "type": "auth",
-      "username": username,
-      "password": password
-    };
-    final response = await _dio.put('/', data: json.encode(request));
-    print("authenticate res ===" + response.data.toString());
-  }
-
-  Future<void> getAuthorization() async {
-    final request = {
-      "client_id": "play-valorant-web-prod",
-      "nonce": "1",
-      "redirect_uri": "https://beta.playvalorant.com/opt_in",
-      "response_type": "token id_token",
-      "scope": "account openid"
-    };
-    final response = await _dio.post('/', data: request);
-    //get cooking from response
-    final cookies = response.headers.map['set-cookie'];
-    if (cookies.isNotEmpty && cookies.length == 2) {
-      final authToken = cookies[1]
-          .split(';')[0]; //it depends on how your server sending cookie
-      //save this authToken in local storage, and pass in further api calls.
-      aToken =
-          authToken; //saving this to global variable to refresh current api calls to add cookie.
-      print("authtoken ===" + authToken);
-    }
-
-    print("cookies ===" + cookies.toString());
-    //print(response.headers.toString());
-
-    print(response.data.toString());
-  }
-}
-
-const Map<String, String> rank = {
+const Map<String, String> rankMap = {
   "0": "Unrated",
   "1": "Unknown 1",
   "2": "Unknown 2",
@@ -112,19 +45,111 @@ class MyApp extends StatefulWidget {
   }
 }
 
+class NetworkService {
+  final JsonDecoder _decoder = new JsonDecoder();
+  final JsonEncoder _encoder = new JsonEncoder();
+
+  Map<String, String> headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+  };
+  Map<String, String> cookies = {};
+
+  void _updateCookie(http.Response response) {
+    String allSetCookie = response.headers['set-cookie'];
+
+    if (allSetCookie != null) {
+      var setCookies = allSetCookie.split(',');
+
+      for (var setCookie in setCookies) {
+        var cookies = setCookie.split(';');
+
+        for (var cookie in cookies) {
+          _setCookie(cookie);
+        }
+      }
+
+      headers['cookie'] = _generateCookieHeader();
+    }
+  }
+
+  void _setCookie(String rawCookie) {
+    if (rawCookie.length > 0) {
+      var keyValue = rawCookie.split('=');
+      if (keyValue.length == 2) {
+        var key = keyValue[0].trim();
+        var value = keyValue[1];
+
+        // ignore keys that aren't cookies
+        if (key == 'path' || key == 'expires') return;
+
+        this.cookies[key] = value;
+      }
+    }
+  }
+
+  String _generateCookieHeader() {
+    String cookie = "";
+
+    for (var key in cookies.keys) {
+      if (cookie.length > 0) cookie += ";";
+      cookie += key + "=" + cookies[key];
+    }
+
+    return cookie;
+  }
+
+  Future<dynamic> put(String url, {body, encoding}) {
+    return http
+        .put(url,
+            body: _encoder.convert(body), headers: headers, encoding: encoding)
+        .then((http.Response response) {
+      final String res = response.body;
+      final int statusCode = response.statusCode;
+
+      _updateCookie(response);
+
+      if (statusCode < 200 || statusCode > 400 || json == null) {
+        throw new Exception("Error while fetching data");
+      }
+      return _decoder.convert(res);
+    });
+  }
+
+  Future<dynamic> post(String url, {body, encoding}) {
+    return http
+        .post(url,
+            body: _encoder.convert(body), headers: headers, encoding: encoding)
+        .then((http.Response response) {
+      final String res = response.body;
+      final int statusCode = response.statusCode;
+
+      _updateCookie(response);
+
+      if (statusCode < 200 || statusCode > 400 || json == null) {
+        throw new Exception("Error while fetching data");
+      }
+      return _decoder.convert(res);
+    });
+  }
+}
+
 class _MyAppState extends State<MyApp> {
-  final TextEditingController _controllerBearer = TextEditingController();
-  final TextEditingController _controllerEntitlement = TextEditingController();
+  final TextEditingController _controllerUsername = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
   String _region = 'na';
   Future<String> _user;
   Future<List<dynamic>> _matches;
+  Future<String> _authURL;
+  String _accessToken;
+  String _entitlementToken;
+  List<int> points = [0, 0, 0];
 
   Future<String> getUserId() async {
     final http.Response response = await http.post(
       'https://auth.riotgames.com/userinfo',
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization': 'Bearer ${_controllerBearer.text}',
+        'Authorization': 'Bearer $_accessToken',
       },
       body: jsonEncode(<String, String>{}),
     );
@@ -136,13 +161,30 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  Future<String> getEntitlementToken() async {
+    final http.Response response = await http.post(
+      'https://entitlements.auth.riotgames.com/api/token/v1',
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $_accessToken',
+      },
+      body: jsonEncode(<String, String>{}),
+    );
+
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      return jsonDecode(response.body)['entitlements_token'];
+    } else {
+      throw Exception('Failed to get user id');
+    }
+  }
+
   Future<List<dynamic>> getCompiDetails(String user) async {
     final http.Response response = await http.get(
       'https://pd.$_region.a.pvp.net/mmr/v1/players/$user/competitiveupdates?startIndex=0&endIndex=20',
       headers: <String, String>{
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${_controllerBearer.text}',
-        'X-Riot-Entitlements-JWT': '${_controllerEntitlement.text}',
+        'Authorization': 'Bearer $_accessToken',
+        'X-Riot-Entitlements-JWT': '$_entitlementToken',
       },
     );
     print(response.statusCode);
@@ -153,6 +195,42 @@ class _MyAppState extends State<MyApp> {
     } else {
       throw Exception('Failed to get compi details');
     }
+  }
+
+  void updateToLatestGames(List<dynamic> matches) {
+    if (matches != null && matches.isNotEmpty) {
+      int count = 0, i = 0;
+      matches.forEach((game) {
+        if (game["CompetitiveMovement"] == "MOVEMENT_UNKNOWN") {
+          // not a ranked game
+        } else if (game["CompetitiveMovement"] == "PROMOTED") {
+          // player promoted
+          int before = game["TierProgressBeforeUpdate"];
+          int after = game["TierProgressAfterUpdate"];
+          int differ = (after - before) + 100;
+          points[i++] = differ;
+          count++;
+        } else if (game["CompetitiveMovement"] == "DEMOTED") {
+          // player demoted
+          int before = game["TierProgressBeforeUpdate"];
+          int after = game["TierProgressAfterUpdate"];
+          int differ = (after - before) - 100;
+          points[i++] = differ;
+          count++;
+        } else {
+          int before = game["TierProgressBeforeUpdate"];
+          int after = game["TierProgressAfterUpdate"];
+          points[i++] = after - before;
+          count++;
+        }
+
+        if (count >= 3) // 3 recent matches found
+          return;
+      });
+
+      //Send Points to Function that changes the UI
+    }
+    setState(() {});
   }
 
   @override
@@ -174,14 +252,12 @@ class _MyAppState extends State<MyApp> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     TextField(
-                      controller: _controllerBearer,
-                      decoration: InputDecoration(
-                          hintText: 'Enter Bearer Authentication Token'),
+                      controller: _controllerUsername,
+                      decoration: InputDecoration(hintText: 'Riot Username'),
                     ),
                     TextField(
-                      controller: _controllerEntitlement,
-                      decoration:
-                          InputDecoration(hintText: 'Entitlement Token'),
+                      controller: _controllerPassword,
+                      decoration: InputDecoration(hintText: 'Password'),
                     ),
                     DropdownButton(
                       isExpanded: true,
@@ -211,20 +287,88 @@ class _MyAppState extends State<MyApp> {
                       child: Text('Check Elo'),
                       onPressed: () async {
                         setState(() {
+                          _user = _user;
+                        });
+                        NetworkService session = NetworkService();
+                        String url =
+                            'https://auth.riotgames.com/api/v1/authorization';
+                        print(await session.post(url, body: {
+                          "client_id": "play-valorant-web-prod",
+                          "nonce": "1",
+                          "redirect_uri":
+                              "https://beta.playvalorant.com/opt_in" + "",
+                          "response_type": "token id_token",
+                          "scope": "account openid"
+                        }));
+                        Map authResponse = await session.put(
+                          url,
+                          body: {
+                            "type": "auth",
+                            "username": _controllerUsername.text,
+                            "password": _controllerPassword.text
+                          },
+                        );
+                        print(authResponse['response']['parameters']['uri']);
+                        String authURL =
+                            authResponse['response']['parameters']['uri'];
+                        setState(() {
+                          _accessToken = RegExp("access_token=(.+?)&scope=")
+                              .stringMatch(authURL)
+                              .split('=')[1]
+                              .split('&')[0];
+                        });
+
+                        print(
+                            "=================== accesss token ===================");
+                        print(_accessToken);
+                        _entitlementToken = await getEntitlementToken();
+                        setState(() {});
+                        print(
+                            "=================== entitlement token ===================");
+                        print(_entitlementToken);
+                        setState(() {
                           _user = getUserId();
                           print("clicked");
                         });
                         _matches = getCompiDetails(await _user);
+                        updateToLatestGames(await _matches);
                         setState(() {});
                       },
                     ),
                   ],
                 )
-              : FutureBuilder<String>(
-                  future: _user,
+              : FutureBuilder<List>(
+                  future: _matches,
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
-                      return Text(snapshot.data);
+                      return Column(
+                        children: [
+                          Text(
+                            'Rank : ' +
+                                rankMap[snapshot.data.first['TierAfterUpdate']
+                                    .toString()],
+                          ),
+                          Text(
+                            'Competitive Movement : ' +
+                                snapshot.data.first['CompetitiveMovement']
+                                    .toString(),
+                          ),
+                          Text(
+                            'Points For last 3 match : ' + points.toString(),
+                          ),
+                          Text(
+                            'Rank Point : ' +
+                                snapshot.data.first['TierProgressAfterUpdate']
+                                    .toString(),
+                          ),
+                          Text('Elo : ' +
+                              ((snapshot.data.first['TierAfterUpdate'] * 100) -
+                                      300 +
+                                      snapshot.data
+                                          .first['TierProgressAfterUpdate'])
+                                  .toString()),
+                        ],
+                      );
                     } else if (snapshot.hasError) {
                       return Text("${snapshot.error}");
                     }
