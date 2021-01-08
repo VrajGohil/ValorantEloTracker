a83ede9360b7cfb316b4cb3f6abc9bdb11f91639
rajbheda5 <rajbheda5@gmail.com>
2021-01-08 20:46:35 +0530

    login page frontend added

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:valo_elo/screens/myapp.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Valorant Elo Tracker',
+      home: MyApp(),
+    );
+  }
 }
