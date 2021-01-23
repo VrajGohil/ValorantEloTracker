b3f549f910f5734bce99917ec6f9a6d050ba7c0b
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2021-01-23 11:26:12 +0530

    error 400 fixed

--- lib/screens/myapp.dart
+++ lib/screens/myapp.dart
@@ -98,6 +98,8 @@ class _MyAppState extends State<MyApp> {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer $_accessToken',
         'X-Riot-Entitlements-JWT': '$_entitlementToken',
+        'X-Riot-ClientPlatform':
+            'ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9'
       },
     );
     print(response.statusCode);
